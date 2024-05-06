cdd0ed123c7ec49e75a2f99313109e27eeda3da8
inf-tatsuya <inf.kuinka@gmail.com>
2024-05-06 11:23:03 +0700

    New/Load Menu Screen

--- Source/Aura/Public/Game/AuraGameModeBase.h
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -6,6 +6,9 @@
 #include "GameFramework/GameModeBase.h"
 #include "AuraGameModeBase.generated.h"
 
+class ULoadScreenSaveGame;
+class USaveGame;
+class UMVVM_LoadSlot;
 class UAbilityInfo;
 class UCharacterClassInfo;
 
@@ -18,10 +21,32 @@ class AURA_API AAuraGameModeBase : public AGameModeBase
 	GENERATED_BODY()
 
 public:
+
+	void SaveSlotData(UMVVM_LoadSlot* LoadSlot, int32 SlotIndex);
+	ULoadScreenSaveGame* GetSaveSlotData(const FString& SlotName, int32 SlotIndex) const;
+	static void DeleteSlot(const FString& SlotName, int32 SlotIndex);
+
+	void TravelToMap(UMVVM_LoadSlot* LoadSlot);
 	
 	UPROPERTY(EditDefaultsOnly, Category = "Character Class Defaults")
 	TObjectPtr<UCharacterClassInfo> CharacterClassInfo;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Ability Info")
 	TObjectPtr<UAbilityInfo> AbilityInfo;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Save Game")
+	TSubclassOf<USaveGame> LoadScreenSaveGameClass;
+
+	UPROPERTY(EditDefaultsOnly)
+	FString DefaultMapName;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<UWorld> DefaultMap;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TMap<FString, TSoftObjectPtr<UWorld>> Maps;
+
+protected:
+
+	virtual void BeginPlay() override;
 };
---
+++ Source/Aura/Public/Game/LoadScreenSaveGame.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "LoadScreenSaveGame.generated.h"
+
+UENUM(BlueprintType)
+enum ESaveSlotStatus
+{
+	Vacant = 0,
+	EnterName = 1,
+	Taken = 2
+};
+
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ULoadScreenSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY()
+	FString SlotName = FString();
+
+	UPROPERTY()
+	int32 SlotIndex = 0;
+
+	UPROPERTY()
+	FString PlayerName = FString("Default Name");
+
+	UPROPERTY()
+	FString MapName = FString("Default Map Name");
+
+	UPROPERTY()
+	TEnumAsByte<ESaveSlotStatus> SaveSlotStatus = Vacant;
+};
